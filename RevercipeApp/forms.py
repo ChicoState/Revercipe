052ac7af847d00aad0be127dc08d73d6f68f3e7c
BFlorek95 <brettflorek@gmail.com>
2020-11-14 17:38:34 -0800

    modify recipe card resolve #39

--- RevercipeApp/forms.py
+++ RevercipeApp/forms.py
@@ -34,22 +34,30 @@ class RegistrationForm(UserCreationForm):
         if commit:
             user.save()
         return user
-        
-class topSearchForm(forms.Form):
-    choice = (
-    ("1", "Recipe"),
-    ("2", "Category"),
-    ("3", "Ingredients")
-    )
 
-    search_by = forms.ChoiceField(label='Pick Search', choices = choice)
-    search_value = forms.CharField(label='Search Value', required=True, max_length=100)
+class top_search_form(forms.Form):
+    choice = (
+    ("1", "Recipes"),
+    ("2", "Category"),
+    ("3", "Ingredient")
+    )
+    #widget=forms.Textarea(attrs={'class':'special', 'size': '40'})
+    search_by = forms.ChoiceField(label='', required=False, choices = choice)
+    search_value = forms.CharField(label='', required=False, max_length=100)
+
+    def getResults(self):
+        data = self.cleaned_data["search_value"]
+        return data
+
+    def getType(self):
+        data = self.cleaned_data["search_by"]
+        return data
 
 class RecipeForm(forms.Form):
     name = forms.CharField(label='Recipe Name',max_length = 100)
     description = forms.CharField(label='Recipe Description', max_length=500, widget=forms.Textarea(attrs={'rows':4, 'cols':5}))
     image = forms.ImageField(label = 'Recipe Photo', required=False)
-    
+
     def save(self, request, commit=True):
         new_recipe = models.RecipeModel(
             name = self.cleaned_data["name"],
@@ -63,8 +71,9 @@ class RecipeForm(forms.Form):
 
 class IngredientForm(forms.Form):
     name = forms.CharField(label='Ingredient Name',max_length = 100)
+    # name = forms.CharField(label='Ingredient Name',max_length = 100, widget=forms.TextInput(attrs={'id':'name'}))
     calories = forms.IntegerField(label='Calorie Amount')
-  
+
     def save(self, request, commit=True):
         new_ingredient = models.RecipeModel(
             name = self.cleaned_data["name"],
@@ -74,5 +83,3 @@ class IngredientForm(forms.Form):
         if commit:
             new_ingredient.save()
         return new_ingredient
-
-
