6005cd6f310b5eded5a9301497ab99be25320363
Brent Deaver <bmdeaver18@gmail.com>
2020-12-03 20:42:31 -0800

    closes #40, Recipe creator can now edit/delete recipe

--- RevercipeApp/urls.py
+++ RevercipeApp/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('settings/', views.settings),
     path('profile/<int:user_id>/', views.profile_view),
     path('create_recipe/', views.create_recipe),
+    path('edit_recipe/<int:instance_id>/', views.edit_recipe),
+    path('delete_recipe/<int:instance_id>/', views.delete_recipe),
     path('recipe/<int:instance_id>/', views.get_recipe),
     path('add_ingredient/<int:instance_id>/', views.add_ingredients),
     path('nutrition/<int:instance_id>/', views.add_nutrition),
